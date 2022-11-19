3c49ce7b619acb20d6ee87678a2b0c2d81895598
tbolkova <tbolkova@student.42wolfsburg.de>
2022-11-19 13:38:48 +0100

    new

--- Libraries/head/fractol.h
+++ Libraries/head/fractol.h
@@ -6,12 +6,22 @@
 /*   By: tbolkova <tbolkova@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:57:32 by tbolkova          #+#    #+#             */
-/*   Updated: 2022/11/17 15:30:05 by tbolkova         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:31:02 by tbolkova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef FRACTOL_H
 #define FRACTOL_H
+
+
+typedef struct	s_data{			// This is a structure. It allows us to put data items of different kinds
+	void	*img;				// under one variable. To access any member of a structure, we use the
+	char	*addr;				// member (.) operator. The member access operator is coded as a period
+	int		bits_per_pixel;		// between the structure variable name and the structure member that we
+	int		line_length;		// wish to access. You would use 'struct'keyword to define variables of
+	int		endian;				// structure type.
+}				t_data;			//
+
 
 #include "../../mlx/mlx.h"
 #include "../Ft_printf/ft_printf.h"
